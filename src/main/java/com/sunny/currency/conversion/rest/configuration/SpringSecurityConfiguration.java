6fb4367505ba976580b252153f99e447c3fe036a
Sunny Das <das.sunny@gmail.com>
2019-11-17 12:11:43 +0530

    Demostrating the use of actuator in sample app.

---
+++ src/main/java/com/sunny/currency/conversion/rest/configuration/SpringSecurityConfiguration.java
@@ -0,0 +1,16 @@
+package com.sunny.currency.conversion.rest.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@Configuration
+public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles("ADMIN");
+    }
+}
